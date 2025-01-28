6fc633370ca8c1adcbceb20736f45879ed4c7135
Eytes <yura.shamanov2000@bk.ru>
2025-01-28 19:55:09 +0200

    исправление для локального запуска

--- parsers/main.py
+++ parsers/main.py
@@ -9,4 +9,4 @@ app.include_router(mvideo_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app=app, host="0.0.0.0", port=8080, workers=4)
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8080, reload=True)
